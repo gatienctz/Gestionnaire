38311279ed8d0ee96295fdf65e415fd064ab8678
abirradi <76118749+abirradi@users.noreply.github.com>
2020-12-23 18:19:40 +0100

    Add files via upload

---
+++ Gestionnaire/UsbDevice.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Forms;
+using System.Management;
+using System.ComponentModel;
+
+
+namespace Gestionnaire
+{
+    public partial class UsbDevice : Form 
+    {
+        public UsbDevice()
+        {
+            InitializeComponent();
+            
+           
+        }
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
+            MessageBox.Show(driveName + " inserted");
+        }
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+             string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
+             MessageBox.Show(driveName + " removed");
+        }
+        void bgwDriveDetector_DoWork(object sender, DoWorkEventArgs e)
+        {
+             var insertQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+             var insertWatcher = new ManagementEventWatcher(insertQuery);
+             insertWatcher.EventArrived += DeviceInsertedEvent;
+             insertWatcher.Start();
+        
+             var removeQuery = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3");
+             var removeWatcher = new ManagementEventWatcher(removeQuery);
+             removeWatcher.EventArrived += DeviceRemovedEvent;
+             removeWatcher.Start();
+        }
+        
+        private static USBDeviceInfo MapEventArgs(EventArrivedEventArgs e)
+        {
+             ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+        
+             string deviceId = (string)instance.GetPropertyValue("DeviceID");
+             string serialNr = deviceId.Substring(deviceId.LastIndexOf('\\')).Replace("\\", "");
+             char driveLetter = GetDriveLetter(serialNr).First();
+        
+             return new USBDeviceInfo(deviceId, serialNr, driveLetter);
+        }
+
+    }
+}
