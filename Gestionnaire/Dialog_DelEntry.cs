c27e1aaeed5be0e9a899c5c0c883961fcac7a3be
abirradi <76118749+abirradi@users.noreply.github.com>
2020-12-29 20:17:25 +0100

    Ajout du formulaire de suppression d'entrée

---
+++ Gestionnaire/Dialog_DelEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Gestionnaire
+{
+    public partial class Dialog_DelEntry : Form
+    {
+        public Dialog_DelEntry()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
