7ddca543a2ec9a1e9fd8fe7deb65f04302beb846
Gatien CONTINSOUZAS <45015039+LariXii@users.noreply.github.com>
2021-01-05 19:18:24 +0100

    Gatien

--- Gestionnaire/manager/AESManager.cs
+++ Gestionnaire/manager/AESManager.cs
@@ -23,6 +23,26 @@ namespace Gestionnaire.manager
 
             return data;
         }
+
+        private static RijndaelManaged CreateRijndaelManaged(byte[] password, byte[] salt)
+        {
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(password, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            //AES.Mode = CipherMode.CFB;
+
+            return AES;
+        }
         
         //Get an XML Document then encrypt it with AES Encryption in a file 
         public static void EncryptXmlDocumentToFile(XmlDocument xmlDoc, string filePath, string password)
@@ -36,22 +56,9 @@ namespace Gestionnaire.manager
             FileStream fsCrypt = new FileStream(filePath, File.Exists(filePath) ? FileMode.Truncate : FileMode.Create);
 
             //convert password string to byte arrray
-            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
 
-            //Set Rijndael symmetric encryption algorithm
-            RijndaelManaged AES = new RijndaelManaged();
-            AES.KeySize = 256;
-            AES.BlockSize = 128;
-            AES.Padding = PaddingMode.PKCS7;
-
-            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
-            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(AES.KeySize / 8);
-            AES.IV = key.GetBytes(AES.BlockSize / 8);
-
-            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
-            //AES.Mode = CipherMode.CFB;
+            RijndaelManaged AES = CreateRijndaelManaged(passwordBytes, salt);
 
             // write salt to the begining of the output file, so in this case can be random every time
             fsCrypt.Write(salt, 0, salt.Length);
@@ -59,19 +66,20 @@ namespace Gestionnaire.manager
             CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
 
             MemoryStream memoryStream = new MemoryStream();
+            //Save the XMLDoc into a memoryStream
             xmlDoc.Save(memoryStream);
+            //Flush the stream and put the position at 0
             memoryStream.Flush();
             memoryStream.Position = 0;
             
             //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
             byte[] buffer = new byte[1048576];
             int read;
-            int total = 0;
+            //Read the memoruStream's byte and the cryptoStream write them crypted into the fileStream
             try
             {
                 while ((read = memoryStream.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    total += read;
                     cs.Write(buffer, 0, read);
                 }
                 // Close up
@@ -94,16 +102,12 @@ namespace Gestionnaire.manager
             byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
             byte[] salt = new byte[32];
 
+            //Open the crypted file 
             FileStream fsCrypt = new FileStream(filePath, FileMode.Open);
-            var position = fsCrypt.Read(salt, 0, salt.Length);
+            //Read the salt's byte
+            fsCrypt.Read(salt, 0, salt.Length);
 
-            RijndaelManaged AES = new RijndaelManaged();
-            AES.KeySize = 256;
-            AES.BlockSize = 128;
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(AES.KeySize / 8);
-            AES.IV = key.GetBytes(AES.BlockSize / 8);
-            AES.Padding = PaddingMode.PKCS7;
+            RijndaelManaged AES = CreateRijndaelManaged(passwordBytes, salt);
             
             CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
 
@@ -114,15 +118,15 @@ namespace Gestionnaire.manager
 
             int read;
             byte[] buffer = new byte[1048576];
-            int total = 0;
+            //Read the cryptoStream that read the file's byte and write decrypted byte into the memoryStream 
             try
             {
                 cs.Read(buffer, 0, 3);
                 while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    total += read;
                     memoryStream.Write(buffer, 0, read);
                 }
+                //Save into the xmlDocument the memoryStream's byte converted to string
                 xmlDocument.LoadXml(Encoding.Default.GetString(memoryStream.ToArray()));
             }
             catch (CryptographicException ex_CryptographicException)
@@ -134,6 +138,7 @@ namespace Gestionnaire.manager
                 Console.WriteLine("Error: " + ex.Message + " linenumber : "+ ex.LineNumber +" lineposition : " + ex.LinePosition);
             }
 
+            //Close the differents streams
             try
             {
                 cs.Close();
